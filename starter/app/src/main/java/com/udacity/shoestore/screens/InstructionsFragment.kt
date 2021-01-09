239ec0c0abc9f84c1c66b2c558d96a590f7b1179
Marcelo Morais <m2marcelo@gmail.com>
2021-01-09 15:39:33 +0100

    Adding the navigation flow for all fragments

--- starter/app/src/main/java/com/udacity/shoestore/screens/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/InstructionsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
@@ -21,6 +22,11 @@ class InstructionsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+
+        binding.buttonReady.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_instructionsFragment_to_shoeListFragment)
+        )
+
         return binding.root
     }
 }
