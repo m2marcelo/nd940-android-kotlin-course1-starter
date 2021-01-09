17bfe2b37d33021496613ab3181e60dd1e685b38
Marcelo Morais <m2marcelo@gmail.com>
2021-01-09 14:03:36 +0100

    Adding a new package for the screens, moved the fragments to there.

--- starter/app/src/main/java/com/udacity/shoestore/screens/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/InstructionsFragment.kt
@@ -1,4 +1,4 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.screens
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
 
