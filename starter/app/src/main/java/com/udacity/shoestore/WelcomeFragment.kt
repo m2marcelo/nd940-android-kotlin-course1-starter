b8a2cdac890464564f7237027da2e636f203a912
Marcelo Morais <m2marcelo@gmail.com>
2021-01-09 13:50:37 +0100

    Adding navigation to instructions fragment

--- starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -18,6 +19,12 @@ class WelcomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        binding.buttonLetsStart.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_instructionsFragment)
+        )
         return binding.root
     }
+
+
 }
